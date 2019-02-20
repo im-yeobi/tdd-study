a10fa5bd10418dc1772ee72413a6e49298922702
jongyeob-Kim <dev.jongyeobKim@gmail.com>
2019-02-20 10:23:15 +0900

    tdd-lotto Test 코드 추가
     - README.md 추가

--- tdd-lotto/src/main/java/Lotto.java
+++ tdd-lotto/src/main/java/Lotto.java
@@ -2,26 +2,35 @@ import java.util.Random;
 
 public class Lotto {
 
-    public int[][] createRandomUserLottoNum(int N) {
-        int[][] userLottoNum = new int[N][7];
+    int[][] userLottoNum;
+    int[] lottoNum;
+
+    public int[] lottoProgram(int N) {
+        createRandomUserLottoNum(N);
+        createRandomLottoNum();
+
+        //  등수통계, 1~7등
+        int[] resultStatistics = new int[7];
+
+        return resultStatistics;
+    }
+
+    private void createRandomUserLottoNum(int N) {
+        userLottoNum = new int[N][7];
         Random generator = new Random();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < userLottoNum[i].length; j++) {
                 userLottoNum[i][j] = generator.nextInt(45) + 1;
             }
         }
-
-        return userLottoNum;
     }
 
-    public int[] createRandomLottoNum() {
-        int[] lottoNum = new int[7];
+    private void createRandomLottoNum() {
+        lottoNum = new int[7];
         Random generator = new Random();
         for (int i = 0; i < lottoNum.length; i++) {
             lottoNum[i] = generator.nextInt(45) + 1;
         }
-
-        return lottoNum;
     }
 
 }
--- tdd-lotto/src/test/java/LottoTest.java
+++ tdd-lotto/src/test/java/LottoTest.java
@@ -3,19 +3,26 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
-
     @Test
-    public void createRandomUserLottoNum() {
-        // 사용자 로또 번호 생성
+    public void lottoProgram() {
         Lotto lotto = new Lotto();
-        assertThat(lotto.createRandomUserLottoNum(10).length).isEqualTo(10);
+        assertThat(lotto.lottoProgram(10).length).isEqualTo(7);
     }
 
-    @Test
-    public void createRandomLottoNum() {
-        // 로또 번호 생성
-        Lotto lotto = new Lotto();
-        assertThat(lotto.createRandomLottoNum().length).isEqualTo(7);
-    }
+//    @Test
+//    public void createRandomUserLottoNum() {
+//        // 사용자 로또 번호 생성
+//        Lotto lotto = new Lotto();
+//        assertThat(lotto.createRandomUserLottoNum(10).length).isEqualTo(10);
+//    }
+//
+//    @Test
+//    public void createRandomLottoNum() {
+//        // 로또 번호 생성
+//        Lotto lotto = new Lotto();
+//        assertThat(lotto.createRandomLottoNum().length).isEqualTo(7);
+//    }
+
+
 
 }
